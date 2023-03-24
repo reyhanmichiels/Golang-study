bf5dfd071f7ca5df99b44065881d8cd613c38496
reyhanmichiels <michiels.reyhan@gmail.com>
2023-03-24 07:06:02 +0700

    Learn constant

---
+++ 4-const/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+//constant declaration
+const firstName string = "reyhan"
+const lastName  = "rusyard"
+
+//multi constant declaration
+const(
+	age int = 20
+	college = "Brawijaya University"
+)
+
+func main() {
+	fmt.Print("Hello my name is ", firstName, " ", lastName,  ", I'm ",  age, " years old, and im study at ", college, "\n")
+}
