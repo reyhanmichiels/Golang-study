6734e776e53e576f9fff506b6c93395bc2b6a6c8
reyhanmichiels <michiels.reyhan@gmail.com>
2023-03-24 10:02:12 +0700

    learn Loop

---
+++ 6-loop/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//basic for loop
+	count := 5
+	for i := 1; i <= count; i++ {
+		fmt.Println("angka", count)
+	}
+
+	//for loop only condition
+	count = 6
+	for count <= 10 {
+		fmt.Println("angka", count)
+		count++
+	}
+
+	//for loop without argument
+	count = 11
+	for {
+		fmt.Println("angka", count)
+		if count == 20 {
+			break
+		}
+		count++
+	}
+}
