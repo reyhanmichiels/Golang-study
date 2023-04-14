df8a7fce0b2e37b0d92f54e7548689e6a18b831c
reyhanmichiels <michiels.reyhan@gmail.com>
2023-04-14 12:13:05 +0700

    task: working exercise 1.4

--- GoProgrammingLanguage/ch1/ex1.4/main.go
+++ GoProgrammingLanguage/ch1/ex1.4/main.go
@@ -6,34 +6,49 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	counts := make(map[string]int)
+	occurs := make(map[string]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, "Standard Input", occurs)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
 			if err != nil {
-				fmt.Fprintf(os.Stderr , "dup2: %v\n", err)
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, arg, occurs)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("there is %d %q at:\n", n, line)
+			fmt.Println(occurs[line])
 		}
-	}	
-}
-
-func countLines(f *os.File, counts map[string]int){
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++;
 	}
 }
+
+func countLines(f *os.File, counts map[string]int, name string, occurs map[string]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+
+		if ! strings.Contains(occurs[input.Text()], name) {
+			temp := fmt.Sprintf(" %s\n", name)
+			occurs[input.Text()] += temp
+		}
+	}
+}
+
+// func countLines(f *os.File, counts map[string]int){
+// 	input := bufio.NewScanner(f)
+// 	for input.Scan() {
+// 		counts[input.Text()]++;
+// 	}
+// }
